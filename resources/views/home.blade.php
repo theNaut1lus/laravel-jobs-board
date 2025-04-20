2dadd5144a8173505bad913d99a52e7d16573c8d
theNaut1lus <sidneyaulakh@gmail.com>
2025-04-20 19:29:54 +1000

    initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <nav>
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <a href="/contact">Contact</a>
+    </nav>
+    <h1>Hello from home page</h1>
+</body>
+
+</html>
