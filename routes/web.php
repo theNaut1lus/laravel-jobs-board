bf4707850effee0038f90914c701d0a0feddd8f1
theNaut1lus <sidneyaulakh@gmail.com>
2025-04-21 17:22:34 +1000

    use route resource to super simplify job route

--- routes/web.php
+++ routes/web.php
@@ -11,18 +11,20 @@ Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 
 
+Route::resource('jobs', JobController::class, [
+    'except' => ''
+]);
 
+// Route::controller(JobController::class)->group(function () {
+//     //job views
+//     Route::get('/jobs', 'index'); //show all jobs
+//     Route::get('/jobs/create', 'create'); //create a job
+//     Route::get('/jobs/{job}', 'show'); //Show a job
+//     Route::get('/jobs/{job}/edit', 'edit'); //Edit View
 
-Route::controller(JobController::class)->group(function () {
-    //job routes
-    Route::get('/jobs', 'index'); //show all jobs
-    Route::get('/jobs/create', 'create'); //create a job
-    Route::get('/jobs/{job}', 'show'); //Show a job
-    Route::get('/jobs/{job}/edit', 'edit'); //Edit View
-
-    //Job CRUD
-    Route::post('/jobs', 'store'); //store Job
-    Route::patch('/jobs/{job}', 'update'); //Update Job
-    Route::delete('/jobs/{job}', 'destroy'); //Delete Job
-});
+//     //Job CRUD
+//     Route::post('/jobs', 'store'); //store Job
+//     Route::patch('/jobs/{job}', 'update'); //Update Job
+//     Route::delete('/jobs/{job}', 'destroy'); //Delete Job
+// });
 
