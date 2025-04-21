4b7e6abc73429c093f71dff6ed09ff7f8add7aa0
theNaut1lus <sidneyaulakh@gmail.com>
2025-04-21 12:09:44 +1000

    emplyer job relationship functions hasmany belongsto

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -14,5 +14,8 @@ class Job extends Model
     protected $table = 'job_listings'; // Specify the table name if it doesn't follow Laravel's naming convention
     protected $fillable = ['title', 'salary']; // Specify the fillable attributes, that can be mass assigned, rest attributes will be guarded.
 
-    //since we extended the Model class, we can use the Eloquent ORM to interact with the database. so no need to have static methods.1
+    public function employer()
+    {
+        return $this->belongsTo(Employer::class);
+    }
 }
