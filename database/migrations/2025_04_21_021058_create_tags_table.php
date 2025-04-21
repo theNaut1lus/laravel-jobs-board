c6fe8225bbb15cf173fc8e0eae5f4b3668f4c293
theNaut1lus <sidneyaulakh@gmail.com>
2025-04-21 12:46:47 +1000

    Tag model, factory, migration and belongtomany relationship

---
+++ database/migrations/2025_04_21_021058_create_tags_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Job;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tags', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        //pivot table mapping jobs and tags.
+        Schema::create('job_tag', function (Blueprint $table) {
+            $table->id();
+            //constraints added to force delete the job_tage relationship if either the job or tag is deleted. this contraint is followed only in the laravel app.
+            $table->foreignIdFor(Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Tag::class)->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tags');
+        Schema::dropIfExists('job_tag');
+    }
+};
