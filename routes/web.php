4c4a6c9387cd2490ad0ff845a2791351d0426bd3
theNaut1lus <sidneyaulakh@gmail.com>
2025-04-21 17:14:29 +1000

    route view for static pages and route group for JobController

--- routes/web.php
+++ routes/web.php
@@ -5,44 +5,24 @@ use Illuminate\Support\Facades\Route;
 
 
 //base app routes
-Route::get("/", function () {
 
-    return view('home');
+Route::view('/', 'home');
+Route::view('/about', 'about');
+Route::view('/contact', 'contact');
+
+
+
+
+Route::controller(JobController::class)->group(function () {
+    //job routes
+    Route::get('/jobs', 'index'); //show all jobs
+    Route::get('/jobs/create', 'create'); //create a job
+    Route::get('/jobs/{job}', 'show'); //Show a job
+    Route::get('/jobs/{job}/edit', 'edit'); //Edit View
+
+    //Job CRUD
+    Route::post('/jobs', 'store'); //store Job
+    Route::patch('/jobs/{job}', 'update'); //Update Job
+    Route::delete('/jobs/{job}', 'destroy'); //Delete Job
 });
-Route::get('/about', function () {
-    return view('about');
-});
 
-Route::get('/contact', function () {
-    return view('contact');
-});
-
-
-//job routes
-
-//show all jobs
-Route::get('/jobs', [JobController::class, 'index']);
-
-//create a job
-Route::get('/jobs/create', [JobController::class, 'create']);
-
-//Show a job
-//used model binding to get the exact model needed by referncing the type in the action function
-Route::get('/jobs/{job}', [JobController::class, 'show']);
-
-//Edit View
-Route::get('/jobs/{job}/edit', [JobController::class, 'edit']);
-
-
-//Job CRUD
-
-//store Job
-Route::post('/jobs', [JobController::class, 'store']);
-
-
-//Update Job
-//Model binding now in effect
-Route::patch('/jobs/{job}', [JobController::class, 'update']);
-
-//Delete Job
-Route::delete('/jobs/{job}', [JobController::class, 'destroy']);
